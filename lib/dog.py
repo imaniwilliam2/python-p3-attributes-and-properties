38f6529c9710d3bc57db1b4257bd0e1329355633
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-03-12 17:15:57 -0400

    tests passing

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,37 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    def __init__(self, name="Fido", breed="Pug"):
+        self.name = name
+        self.breed = breed
+
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+    name = property(get_name, set_name)
+
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            pass
+            print("Breed must be in list of approved breeds.")
+        
+    breed = property(get_breed, set_breed)
+
+
+
+    
+
